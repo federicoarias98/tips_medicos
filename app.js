c5e3324a68d1335d5ebeefbde988b67efb98b245
Jeronimo <jeronimo@pop-os.bbrouter>
2024-09-15 00:12:55 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,78 @@
+let horas = 7;
+let minutos = 0;
+let segundos = 0;
+cargarSegundo();
+
+//  Definir y ejecutar los segundos 
+function cargarSegundo(){ 
+    let txtSegundos;
+
+    if (segundos < 0){
+        segundos = 59;
+    }
+
+    //Mostrar segundos
+    if (segundos < 10){
+        txtSegundos = `0${segundos}`;
+    }else{ 
+        txtSegundos = segundos;
+    }
+    document.getElementById('segundos').innerHTML = txtSegundos;
+    segundos --;
+    
+    cargarMinutos(segundos);
+}
+
+// Definir y ejecutar minutos
+function cargarMinutos(segundos){ 
+    let txtMinutos;
+    
+    if (segundos == -1 && minutos !== 0){
+        setTimeout(() => {
+            minutos--;
+        }, 500)
+    } else if (segundos == -1 && minutos == 0){ 
+         setTimeout(() => {
+            minutos = 59;
+        }, 500)
+    }
+
+    //Mostrar minutos
+    if (minutos < 10){
+        txtMinutos = `0${minutos}`;
+    }else{ 
+        txtMinutos = minutos;
+    }
+    document.getElementById('minutos').innerHTML = txtMinutos;
+    cargarHoras(segundos,minutos);
+ 
+}
+
+//Definir y ejecutar horas
+function cargarHoras(segundos, minutos){
+    let txtHoras;
+
+     if (segundos == -1 && minutos == 0 && horas !== 0){
+        setTimeout(() => {
+            horas--;
+        }, 500)
+    } else if (segundos == -1 && minutos == 0 && horas == 0){ 
+        setTimeout(() =>{ 
+            horas = 2;
+        },500)
+    }
+
+    //Mostrar horas
+    if (horas < 10) {
+        txtHoras = `0${horas}`;
+    }else{ 
+        txtHoras = horas;
+    }
+    document.getElementById('horas').innerHTML = txtHoras;
+    
+}
+    
+
+
+//Ejecutar cada segundo
+setInterval(cargarSegundo, 1000);
